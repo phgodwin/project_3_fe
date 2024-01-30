3214bc4b6e3ee38e9402a40c91e3be3b2dab54a0
unknown <agustinadipaolauk@gmail.com>
2024-01-30 22:20:15 +0000

    update video home page

--- src/Components/Routing/Home.jsx
+++ src/Components/Routing/Home.jsx
@@ -1,5 +1,6 @@
-import Team from "../../MicrosoftTeams-image.png";
+// import Team from "../../MicrosoftTeams-image.png";
 import HomeTotals from '../HomeTotals';
+import videp from "../../videp.mp4";
 
 
 function Home() {
@@ -25,10 +26,13 @@ function Home() {
 
                 <br/>
                 <br/>
+                <div>
                 <h1>Meet the Team!</h1>
-                <a><img className='img-fluid' src={Team} alt='Team Photo' width="50%" /></a>
+                <video width="70%" height ="40%" controls>
+                    <source src = {videp} type='video/mp4'  />
+                    </video>
                 <br />
-
+                </div>
                 <p>Left to right: Liliano Balde, Paige Godwin, Nathan Liddle, Richard Miles, Andrew King </p>
 
             </article>
