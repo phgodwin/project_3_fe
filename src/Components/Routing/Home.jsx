c8b06208824035eb45885d550e637ccf46959cd4
Craigm84 <craigmorris2907@gmail.com>
2024-02-02 15:27:50 +0000

    Names Updated

--- src/Components/Routing/Home.jsx
+++ src/Components/Routing/Home.jsx
@@ -35,38 +35,49 @@ function Home() {
                     </video>
                 <br />
                 </div>
-                <p>Left to right: Liliano Balde, Paige Godwin, Nathan Liddle, Richard Miles, Andrew King </p>
+                <p style={{textDecoration: "line-through", color: "red"}}>Left to right: Liliano Balde, Paige Godwin, Nathan Liddle, Richard Miles, Andrew King</p>
+                <h3 style={{color: "green"}}>Left to right:  Craig Morris, Agustina Di Paola, Nathan Liddle, Anna Wilson </h3>
 
             </article>
 
             <br />
             <section style={{color: "white"}} >
                 <div style={{ backgroundColor: "#984da2", borderRadius: "5px", padding: "5px", margin: "10px"}}>
-                    <h5>Liliano Balde</h5>
+                <h5 >Craig Morris</h5>
+
+                    <del><h5 >Liliano Balde</h5>
                     <p>"I bring pure european excellence. If you want a property secured like CR7 secures the
-                        ballon dor then you can trust me to make your dreams come true. SIUUUU"</p>
+                        Ballon d'or then you can trust me to make your dreams come true."</p></del>
                 </div>
                 <div style={{ backgroundColor: "#984da2", borderRadius: "5px", padding: "5px", margin: "10px"}}>
-                    <h5>Paige Godwin</h5>
-                    <p>"A good estate agent puts the 'pro' in 'property' and the 'fun' in 'funding'!"</p>
+                <h5 >Agustina Di Paola</h5>
+
+                    <del><h5>Paige Godwin</h5>
+                    <p>"A good estate agent puts the 'pro' in 'property' and the 'fun' in 'funding'!"</p></del>
                 </div>
                 <div style={{ backgroundColor: "#984da2", borderRadius: "5px", padding: "5px", margin: "10px"}}>
-                    <h5>Nathan Liddle</h5>
+                   <del> <h5>Nathan Liddle</h5>
+                    <p>"I’m a totally rad estate agent with 10 years of experience in
+                        the industry. I’m like a superhero! So, if you’re looking
+                        for an agent who’s as cool as a cucumber and as smooth as butter, look no further!"</p></del>
+                        <h5>Nathan Liddle</h5>
                     <p>"I’m a totally rad estate agent with 10 years of experience in
                         the industry. I’m like a superhero! So, if you’re looking
                         for an agent who’s as cool as a cucumber and as smooth as butter, look no further!"</p>
                 </div>
                 <div style={{ backgroundColor: "#984da2", borderRadius: "5px", padding: "5px", margin: "10px"}}>
-                    <h5>Richard Miles</h5>
+                <h5 >Anna Wilson</h5>
+                
+                <del><h5>Richard Miles</h5>
                     <p>"Bringing a touch of flair to the world of real estate at Control Alt Elite! As an
                         estate agent, I spin property magic, turning dreams into deeds. With a knack for
                         finding the perfect match and a knackier skill in negotiation, I'm the Sherlock Holmes
-                        of homes. Let's embark on this adventure together and make your property dreams a reality!"</p>
+                        of homes. Let's embark on this adventure together and make your property dreams a reality!"</p></del>
                 </div>
                 <div style={{ backgroundColor: "#984da2", borderRadius: "5px", padding: "5px", margin: "10px"}}>
-                    <h5>Andrew King</h5>
+                    <del><h5>Andrew King</h5>
                     <p>"The 'King' of the property world as named by some but in reality im just a truly humble man giving
-                        families homes whilst raking in commission."</p>
+                        families homes whilst raking in commission."</p></del>
                 </div>
             </section>
 
